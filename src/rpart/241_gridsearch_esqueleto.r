76e5546e6e50bfb71e37b463b70cb65080dd443a
MaCatalinaBadano <mbadano@itba.edu.ar>
2023-09-19 04:12:24 +0000

    241 primero

--- src/rpart/241_gridsearch_esqueleto.r
+++ src/rpart/241_gridsearch_esqueleto.r
@@ -89,7 +89,7 @@ ArbolesMontecarlo <- function(semillas, param_basicos) {
 #------------------------------------------------------------------------------
 
 # Aqui se debe poner la carpeta de la computadora local
-setwd("X:\\gdrive\\ITBA2023B\\") # Establezco el Working Directory
+setwd("~/buckets/b1/") # Establezco el Working Directory
 # cargo los datos
 
 # cargo los datos
