dbaf921cf25dbc14be2197271755729fcf3d6846
MaCatalinaBadano <mbadano@itba.edu.ar>
2023-09-25 16:16:13 +0000

    101_050

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -21,8 +21,8 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.5, # esto significa no limitar la complejidad de los splits
-        minsplit = 200, # minima cantidad de registros para que se haga el split
+        cp = -1, # esto significa no limitar la complejidad de los splits
+        minsplit = 20, # minima cantidad de registros para que se haga el split
         minbucket = 50, # tamaño minimo de una hoja
         maxdepth = 6
 ) # profundidad maxima del arbol
@@ -60,6 +60,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_005.csv",
+        file = "./exp/KA2001/K101_010.csv",
         sep = ","
 )
