3c47f75793c5765f72892abdabfac5b210ebbee5
MaCatalinaBadano <mbadano@itba.edu.ar>
2023-10-04 00:10:32 +0000

    exp_03

--- src/workflow_exp_3/631_FE_historia_03.r
+++ src/workflow_exp_3/631_FE_historia_03.r
@@ -21,12 +21,12 @@ PARAM$experimento <- "FE6310"
 
 PARAM$exp_input <- "DR6210"
 
-PARAM$lag1 <- FALSE
+PARAM$lag1 <- TRUE
 PARAM$lag2 <- TRUE
-PARAM$lag3 <- TRUE
+PARAM$lag3 <- FALSE
 
 PARAM$Tendencias1$run <- TRUE
-PARAM$Tendencias1$ventana <- 4
+PARAM$Tendencias1$ventana <- 6
 PARAM$Tendencias1$tendencia <- TRUE
 PARAM$Tendencias1$minimo <- FALSE
 PARAM$Tendencias1$maximo <- FALSE
@@ -44,7 +44,7 @@ PARAM$Tendencias2$ratioavg <- FALSE
 PARAM$Tendencias2$ratiomax <- FALSE
 
 
-PARAM$RandomForest$run <- FALSE
+PARAM$RandomForest$run <- TRUE
 PARAM$RandomForest$num.trees <- 10
 PARAM$RandomForest$max.depth <- 6
 PARAM$RandomForest$min.node.size <- 2000
