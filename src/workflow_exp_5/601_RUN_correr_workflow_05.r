83219ca2ba0ba3513391d5df64c6f190dffab872
MaCatalinaBadano <mbadano@itba.edu.ar>
2023-10-05 02:26:40 +0000

    exp_05

--- src/workflow_exp_5/601_RUN_correr_workflow_05.r
+++ src/workflow_exp_5/601_RUN_correr_workflow_05.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow_exp_4/611_CA_reparar_dataset_05.r")
-source("~/dm2023b/src/workflow_exp_4/621_DR_corregir_drifting_05.r")
-source("~/dm2023b/src/workflow_exp_4/631_FE_historia_05.r")
-source("~/dm2023b/src/workflow_exp_4/641_TS_training_strategy_05.r")
+source("~/dm2023b/src/workflow_exp_5/611_CA_reparar_dataset_05.r")
+source("~/dm2023b/src/workflow_exp_5/621_DR_corregir_drifting_05.r")
+source("~/dm2023b/src/workflow_exp_5/631_FE_historia_05.r")
+source("~/dm2023b/src/workflow_exp_5/641_TS_training_strategy_05.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow_exp_4/651_HT_lightgbm_05.r")
-source("~/dm2023b/src/workflow_exp_4/661_ZZ_final_05.r")
+source("~/dm2023b/src/workflow_exp_5/651_HT_lightgbm_05.r")
+source("~/dm2023b/src/workflow_exp_5/661_ZZ_final_05.r")
